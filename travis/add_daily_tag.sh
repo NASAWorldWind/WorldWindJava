78b918b9b941a69db3cccc44dbc637dda4089cad
pdavidc <paul@paulcollinssoftware.com>
2016-11-26 14:45:17 -0800

    Updated the Travis CI scripts to match World Wind repository conventions

--- travis/add_daily_tag.sh
+++ travis/add_daily_tag.sh
@@ -1,35 +1,37 @@
 #!/bin/bash
+
 # ======================================================================================================================
-# For Travis builds triggered by a cron job, this script adds a "daily" tag to the HEAD of the checked out branch.
+# Pushes a daily build tag to the HEAD of the checked out branch (usually develop). Does nothing if the build is not
+# initiated by a cron job.
 #
-# The script uses "git" to update tags in the repo. Commands using authentication use --quiet prevent leaking the access
-# token into the log. Also, these are also redirected to /dev/null to prevent leaking the access token in case of error.
-#
-# Syntax: add_daily_tag.sh
+# Uses Git to update tags in the repo. Git commands using authentication are redirected to /dev/null to prevent leaking
+# the access token into the log.
 # ======================================================================================================================
-# Ensure shell commands are not echoed to the log to prevent leaking the access token.
+
+# Ensure shell commands are not echoed to the log to prevent leaking the access token
 set +x
 
-# Ensure the current build is a cron-job
+# Exit without error when this build is not a cron job
 if [[ "$TRAVIS_EVENT_TYPE" != "cron" ]]; then
-    exit 0  # Exit quietly without error
+    exit 0
 fi
 
-# Get the GitHub remote origin URL.
+# Get the GitHub remote origin URL
 REMOTE_URL=$(git config --get remote.origin.url) > /dev/null
-# Add the GitHub authentication token if it's not already embedded in the URL (test if an "@" sign is present).
+# Add the GitHub authentication token if it's not already embedded in the URL (test if an "@" sign is present)
 if [[ $REMOTE_URL != *"@"* ]]; then
-    # Use the stream editor to inject the GitHub authentication token into the remote url after the protocol.
+    # Use the stream editor to inject the GitHub authentication token into the remote url after the protocol
     # Example:  https://github.com/.../repo.git -> https://<token>@github.com/.../repo.git
     REMOTE_URL=$(echo $REMOTE_URL | sed -e "s#://#://$GITHUB_API_KEY@#g") > /dev/null
 fi
 
-# Add a "daily/YYYYMMDD" tag to the HEAD. Using a "/" in the name allows GUI tools to collect all "daily" tags in a folder.
+# Add the tag "daily/YYYYMMDD" tag to the HEAD. Delimiting the date with a slash allows Git tools to collect daily tags in a folder
 # RELEASE_DATE=$(date '+%Y%m%d-%H%M%Z') # add time for testing
 RELEASE_DATE=$(date '+%Y%m%d')
 DAILY_TAG="daily/${RELEASE_DATE}"
 
 # Create a lightweight tag (vs an annotated tag) and push it to the remote
+echo "Pushing daily tag ${DAILY_TAG}"
 git tag $DAILY_TAG
 git push --quiet $REMOTE_URL tag $DAILY_TAG > /dev/null
 
--- travis/update_release.sh
+++ travis/update_release.sh
@@ -1,24 +1,26 @@
 #!/bin/bash
+
 # ======================================================================================================================
-# For Travis builds triggered by a "tag", this script creates or updates a GitHub release and uploads release artifacts.
+# Creates or updates a GitHub release and uploads release artifacts for builds initiated by a tag. Does nothing if the
+# build is not associated with a tag.
 #
-# The script uses "curl" to performs CRUD operations on the GitHub Repos/Releases REST API.
-# The script uses "jq" to process the REST API JSON results.
-#   jq should be installed before this script is executed, e.g., in the .travis.yml "before_script:" block.
+# Uses curl to performs CRUD operations on the GitHub Repos/Releases REST API.
+#
+# Uses jq to process the REST API JSON results. jq should be installed before this script is executed, e.g., in
+# .travis.yml before_script block:
 #       sudo apt-get install -qq jq
-#   For jq documentation, see https://stedolan.github.io/jq/manual/
-# The script uses "git" to update tags in the repo.
+# See https://stedolan.github.io/jq/manual/ for jq documentation.
 #
-# Note: Commands using authentication are redirected to /dev/null to prevent leaking the access token into the log.
-#
-# Syntax: update_release.sh
+# Uses Git to update tags in the repo. Git commands using authentication are redirected to /dev/null to prevent leaking
+# the access token into the log.
 # ======================================================================================================================
+
 # Ensure shell commands are not echoed in the log to prevent leaking the access token.
 set +x
 
 # Ensure the GitHub Personal Access Token for GitHub exists
 if [[ -z "$GITHUB_API_KEY" ]]; then
-    echo $0 error: You must export the GITHUB_API_KEY containing the personal access token for Travis\; GitHub was not updated.
+    echo "$0 error: You must export the GITHUB_API_KEY containing the personal access token for Travis\; GitHub was not updated."
     exit 1
 fi
 
@@ -26,54 +28,38 @@ fi
 RELEASES_URL="https://api.github.com/repos/nasaworldwind/worldwindjava/releases"
 TAGS_URL="https://api.github.com/repos/nasaworldwind/worldwindjava/tags"
 UPLOADS_URL="https://uploads.github.com/repos/nasaworldwind/worldwindjava/releases"
-DOWNLOADS_URL="https://github.com/nasaworldwind/WorldWindJava/releases/download"
+DOWNLOADS_URL="https://github.com/nasaworldwind/worldwindjava/releases/download"
 
-# Get the GitHub remote origin URL.
+# Get the GitHub remote origin URL
 REMOTE_URL=$(git config --get remote.origin.url) > /dev/null
-# Add the GitHub authentication token if it's not already embedded in the URL (test if an "@" sign is present).
+# Add the GitHub authentication token if it's not already embedded in the URL (test if an "@" sign is present)
 if [[ $REMOTE_URL != *"@"* ]]; then
-    # Use the stream editor to inject the GitHub authentication token into the remote url after the protocol.
+    # Use the stream editor to inject the GitHub authentication token into the remote url after the protocol
     # Example:  https://github.com/.../repo.git -> https://<token>@github.com/.../repo.git
     REMOTE_URL=$(echo $REMOTE_URL | sed -e "s#://#://$GITHUB_API_KEY@#g") > /dev/null
 fi
 
-# Human readable release timestamp used in release notes
-RELEASE_DATE=$(date '+%a, %b %e, %Y at %H:%M %Z')
-
-# Initialize the release variables predicated on the tag. Note, on a tagged build, Travis cloned the repo into a branch
-# named with the tag, thus the TRAVIS_BRANCH var reflects the tag name, not "master" or "develop" like you might expect.
-if [[ -n $TRAVIS_TAG ]]; then
-    if [[ "${TRAVIS_TAG}" == "daily"* ]]; then
-        # Variables used to publish a "daily" release
-        echo Preparing the $TRAVIS_TAG daily release
-        RELEASE_TAG=$TRAVIS_TAG
-        RELEASE_NAME="Daily Build"
-        RELEASE_NOTES="World Wind Java daily builds from the [develop](https://github.com/NASAWorldWind/WorldWindJava/tree/develop) branch. "
-        RELEASE_NOTES+="Daily builds have the newest, bleeding-edge World Wind Java SDK features. Intended for developers and early adopters."
-        RELEASE_NOTES+="\r\n\r\nBuilt on ${RELEASE_DATE}."
-        DRAFT="false"
-        PRERELEASE="true"
-    else
-        # Variables to prepare a draft release
-        echo Preparing the $TRAVIS_TAG release
-        RELEASE_TAG=$TRAVIS_TAG
-        RELEASE_NAME=$TRAVIS_TAG
-        RELEASE_NOTES="Built ${RELEASE_DATE}."
-        DRAFT="true"
-        PRERELEASE="false"
-    fi
-else
-    echo $0 Skipping non-tagged build on $TRAVIS_BRANCH branch
-    exit 0  # Exit without error
+# Initialize the release variables predicated on the tag. On a tagged build, Travis cloned the repo into a branch named
+# with the tag, thus the TRAVIS_BRANCH var reflects the tag name, not "master" or "develop" like you might expect.
+if [[ "${TRAVIS_TAG}" == "daily"* ]]; then # daily build associated with a tag in the format daily/YYYYMMDD
+    RELEASE_NAME="Daily Build"
+    DRAFT="false"
+    PRERELEASE="true"
+elif [[ -n $TRAVIS_TAG ]]; then # manually created tag; prepare a draft release
+    RELEASE_NAME=$TRAVIS_TAG
+    DRAFT="true"
+    PRERELEASE="false"
+else # build is not associated with a tag; exit quietly without error
+    exit 0
 fi
+
 
 # ===========================
 # GitHub Release Maintenance
 # ===========================
 
-# Query the release ids for releases with with the given name. If there's more than one, then we'll use the first.
-# Note: in order to see "draft" releases, we need to authenticate with a user that has push access to the repo.
-echo Quering the ids for $RELEASE_NAME
+# Query the release ids for releases with with the given name. If there's more than one, then we'll use the first. In
+# order to see draft releases, we need to authenticate with a user that has push access to the repo.
 RELEASE_ARRAY=( \
     $(curl --silent --header "Authorization: token ${GITHUB_API_KEY}" ${RELEASES_URL} \
     | jq --arg name "${RELEASE_NAME}" '.[] | select(.name == $name) | .id') \
@@ -81,19 +67,17 @@ RELEASE_ARRAY=( \
 
 # Get the first id returned from the query
 RELEASE_ID=${RELEASE_ARRAY[0]}
-echo $RELEASE_NAME release id: $RELEASE_ID
 
 # Update the GitHub releases (create if release id's length == 0)
 if [[ ${#RELEASE_ID} -eq 0 ]]; then
-    # Create a release
-    echo Creating the $RELEASE_NAME Release
+    # Emit a log message for the new release
+    echo "Creating release ${RELEASE_NAME} with tag ${TRAVIS_TAG}"
 
     # Build the JSON (Note: single quotes inhibit variable substitution, must use escaped double quotes)
     # Note: the tag already exists, so the "target_commitish" parameter is not used
     JSON_DATA="{ \
-      \"tag_name\": \"${RELEASE_TAG}\", \
+      \"tag_name\": \"${TRAVIS_TAG}\", \
       \"name\": \"${RELEASE_NAME}\", \
-      \"body\": \"${RELEASE_NOTES}\", \
       \"draft\": ${DRAFT}, \
       \"prerelease\": ${PRERELEASE} \
     }"
@@ -108,18 +92,16 @@ if [[ ${#RELEASE_ID} -eq 0 ]]; then
 
     # Extract the newly created release id from the JSON result
     RELEASE_ID=$(echo $RELEASE | jq '.id')
-    echo New release ID: $RELEASE_ID
 else
-    # Update an existing release
-    echo Updating the $RELEASE_NAME release to tag $RELEASE_TAG
+    # Emit a log message for the updated release
+    echo "Updating release ${RELEASE_NAME} with tag ${TRAVIS_TAG}"
 
-    # Define the patch data to update the tag_name and the timestamped release notes
+    # Define the patch data to update the tag_name
     JSON_DATA="{ \
-        \"tag_name\": \"${RELEASE_TAG}\", \
-        \"body\": \"${RELEASE_NOTES}\" \
+        \"tag_name\": \"${TRAVIS_TAG}\" \
      }"
 
-    # Update GitHub
+    # Update the release on GitHub
     curl --silent \
     --header "Authorization: token ${GITHUB_API_KEY}" \
     --header "Content-Type: application/json" \
@@ -130,7 +112,7 @@ fi
 
 # Assert that we found a GitHub release id for the current branch (release id length > 0)
 if [[ ${#RELEASE_ID} -eq 0 ]]; then
-    echo $0 error: The $RELEASE_NAME release was not found. No artifacts were uploaded to GitHub releases.
+    echo "$0 error: Release $RELEASE_NAME was not found. No artifacts were uploaded to GitHub releases."
     exit 0
 fi
 
@@ -143,6 +125,9 @@ JAR_FILES=( worldwind.jar worldwindx.jar )
 ZIP_FILES=( worldwind-javadoc.zip )
 ALL_FILES=( "${JAR_FILES[@]}" "${ZIP_FILES[@]}" )
 
+# Emit a log message for the updated release
+echo "Uploading release assets for ${RELEASE_NAME}"
+
 # Remove existing assets if they exist (asset id length > 0)
 for FILENAME in ${ALL_FILES[*]}
 do
@@ -152,7 +137,7 @@ do
         | jq --arg filename $FILENAME '.[] | select(.name == $filename) | .id')
 
     if [ ${#ASSET_ID} -gt 0 ]; then
-        echo DELETE ${RELEASES_URL}/assets/${ASSET_ID}
+        echo "Deleting ${FILENAME}"
         curl --silent  \
         --header "Authorization: token ${GITHUB_API_KEY}" \
         --request DELETE ${RELEASES_URL}/assets/${ASSET_ID} > /dev/null
@@ -162,6 +147,7 @@ done
 # Upload the release artifacts
 for FILENAME in ${JAR_FILES[*]}
 do
+    echo "Posting ${FILENAME}"
     curl --silent  \
     --header "Authorization: token ${GITHUB_API_KEY}" \
     --header "Content-Type: application/java-archive" \
@@ -172,6 +158,7 @@ done
 
 for FILENAME in ${ZIP_FILES[*]}
 do
+    echo "Posting ${FILENAME}"
     curl --silent  \
     --header "Authorization: token ${GITHUB_API_KEY}" \
     --header "Content-Type: application/zip" \
@@ -185,8 +172,9 @@ done
 # ======================
 
 # Cleanup daily tags
-echo Cleaning up daily tags
 if [[ "${TRAVIS_TAG}" == "daily"* ]]; then
+    # Emit a log message for the daily tag cleanup
+    echo "Cleaning up daily tags"
 
     # Get a sorted array of daily tags (sorting is req'd if you want to do some array slicing)
     DAILY_TAGS=($(git tag --list "daily*" | sort))
@@ -195,6 +183,7 @@ if [[ "${TRAVIS_TAG}" == "daily"* ]]; then
     for TAG in ${DAILY_TAGS[*]}
     do
         if [[ $TAG != $TRAVIS_TAG ]]; then
+            echo "Deleting ${TAG}"
             git tag --delete $TAG
             git push --quiet $REMOTE_URL :${TAG} > /dev/null
             RESULT=$?
