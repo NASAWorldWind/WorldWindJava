59b008dfd8ce6f7708bf0525665d21d6d3173bc0
pdavidc <paul@paulcollinssoftware.com>
2016-12-06 10:59:49 -0800

    Removed unnecessary final modifiers from MIL-STD-2525 interface constants

--- src/gov/nasa/worldwind/symbology/milstd2525/MilStd2525Constants.java
+++ src/gov/nasa/worldwind/symbology/milstd2525/MilStd2525Constants.java
@@ -22,17 +22,17 @@ public interface MilStd2525Constants
      * The default location that MIL-STD-2525 tactical symbols and tactical point graphics retrieve their icons from:
      * https://worldwind.arc.nasa.gov/milstd2525c/rev1/
      */
-    final String DEFAULT_ICON_RETRIEVER_PATH = "https://worldwind.arc.nasa.gov/milstd2525c/rev1/";
+    String DEFAULT_ICON_RETRIEVER_PATH = "https://worldwind.arc.nasa.gov/milstd2525c/rev1/";
 
     // Color RGB values from MIL-STD-2525C Table XIII, pg. 44.
     /** Default material used to color tactical graphics that represent friendly entities. */
-    final Material MATERIAL_FRIEND = Material.BLACK;
+    Material MATERIAL_FRIEND = Material.BLACK;
     /** Default material used to color tactical graphics that represent hostile entities. */
-    final Material MATERIAL_HOSTILE = new Material(new Color(255, 48, 49));
+    Material MATERIAL_HOSTILE = new Material(new Color(255, 48, 49));
     /** Default material used to color tactical graphics that represent neutral entities. */
-    final Material MATERIAL_NEUTRAL = new Material(new Color(0, 226, 0));
+    Material MATERIAL_NEUTRAL = new Material(new Color(0, 226, 0));
     /** Default material used to color tactical graphics that represent unknown entities. */
-    final Material MATERIAL_UNKNOWN = new Material(new Color(255, 255, 0));
+    Material MATERIAL_UNKNOWN = new Material(new Color(255, 255, 0));
     /** Default material used to color tactical graphics that represent obstacles. */
-    final Material MATERIAL_OBSTACLE = new Material(new Color(0, 226, 0));
+    Material MATERIAL_OBSTACLE = new Material(new Color(0, 226, 0));
 }
