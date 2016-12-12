b762c008825089742985058368c77af9e48316fe
pdavidc <paul@paulcollinssoftware.com>
2016-12-11 19:17:13 -0800

    Increased the run-demo script's max VM memory to 1024 MB

--- run-demo.bash
+++ run-demo.bash
@@ -9,8 +9,6 @@
 #
 # Run a WorldWind Demo
 #
-# $Id: run-demo.bash 1171 2013-02-11 21:45:02Z dcollins $
-#
 
 echo Running $1
-java -Xmx512m -Dsun.java2d.noddraw=true -classpath ./src:./classes:./worldwind.jar:./worldwindx.jar:./jogl-all.jar:./gluegen-rt.jar:./gdal.jar $*
+java -Xmx1024m -Dsun.java2d.noddraw=true -classpath ./src:./classes:./worldwind.jar:./worldwindx.jar:./jogl-all.jar:./gluegen-rt.jar:./gdal.jar $*
--- run-demo.bat
+++ run-demo.bat
@@ -2,8 +2,7 @@ REM Copyright (C) 2012 United States Government as represented by the Administra
 REM National Aeronautics and Space Administration.
 REM All Rights Reserved.
 
-REM Windows Batch file for Running a WorldWind Demo
-REM $Id: run-demo.bat 1171 2013-02-11 21:45:02Z dcollins $
+REM Run a WorldWind Demo
 
 @echo Running %1
-java -Xmx512m -Dsun.java2d.noddraw=true -classpath .\src;.\classes;.\worldwind.jar;.\worldwindx.jar;.\jogl-all.jar;.\gluegen-rt.jar;.\gdal.jar %*
+java -Xmx1024m -Dsun.java2d.noddraw=true -classpath .\src;.\classes;.\worldwind.jar;.\worldwindx.jar;.\jogl-all.jar;.\gluegen-rt.jar;.\gdal.jar %*
