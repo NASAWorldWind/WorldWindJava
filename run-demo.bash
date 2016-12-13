43abc5fb8eb63fea25b5674d80bbb1ba2c490fa9
Bruce Schubert <bruce@emxsys.com>
2016-12-13 10:09:31 -0800

    Updated run-demo scripts to launch the ApplicationTemplate example if a class name is not provided.

--- run-demo.bash
+++ run-demo.bash
@@ -7,8 +7,17 @@
 #
 
 #
+# Default to the ApplicationTemplate example if no arguments are provided
+#
+if [ $# -lt 1 ]
+then
+  WWDEMO=gov.nasa.worldwindx.examples.ApplicationTemplate
+else
+  WWDEMO=$*
+fi
+
+#
 # Run a WorldWind Demo
 #
-
-echo Running $1
-java -Xmx1024m -classpath ./worldwind.jar:./worldwindx.jar:./gdal.jar:./jogl-all.jar:./gluegen-rt.jar $*
+echo Running $WWDEMO
+java -Xmx1024m -classpath "./worldwind.jar;./worldwindx.jar;./gdal.jar;./jogl-all.jar;./gluegen-rt.jar" $WWDEMO
--- run-demo.bat
+++ run-demo.bat
@@ -1,8 +1,11 @@
+@echo off
 REM Copyright (C) 2012 United States Government as represented by the Administrator of the
 REM National Aeronautics and Space Administration.
 REM All Rights Reserved.
 
-REM Run a WorldWind Demo
+REM Default to the ApplicationTemplate example if a class name is not provided
+IF "%1"=="" (SET WWDEMO=gov.nasa.worldwindx.examples.ApplicationTemplate) ELSE (SET WWDEMO=%*)
 
-@echo Running %1
-java -Xmx1024m -Dsun.java2d.noddraw=true -classpath .\worldwind.jar;.\worldwindx.jar;.\gdal.jar;.\jogl-all.jar;.\gluegen-rt.jar %*
+REM Run a WorldWind Demo
+@echo Running %WWDEMO%
+java -Xmx1024m -Dsun.java2d.noddraw=true -classpath .\worldwind.jar;.\worldwindx.jar;.\gdal.jar;.\jogl-all.jar;.\gluegen-rt.jar %WWDEMO%
