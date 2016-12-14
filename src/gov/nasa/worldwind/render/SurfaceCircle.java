d18ec771a10c5b2867d5fbe7d25a4e583a984f54
Wiehann Matthysen <wcmatthysen@gmail.com>
2016-12-14 20:50:20 +0200

    Added a constructor for partial circles.
    Added an additional constructor to the SurfaceCircle class to allow for
    partial circles to be drawn. This constructor accept an additional
    parameter theta, that gets passed to the SurfaceEllipse super
    constructor.

--- src/gov/nasa/worldwind/render/SurfaceCircle.java
+++ src/gov/nasa/worldwind/render/SurfaceCircle.java
@@ -104,6 +104,25 @@ public class SurfaceCircle extends SurfaceEllipse
     {
         super(normalAttrs, center, radius, radius, Angle.ZERO, intervals);
     }
+    
+    /**
+     * Constructs a new surface circle with the specified normal (as opposed to highlight) attributes, the specified
+     * center location,  radius (in meters), and initial number of geometry intervals. Modifying the attribute reference
+     * after calling this constructor causes this shape's appearance to change accordingly.
+     *
+     * @param normalAttrs the normal attributes. May be null, in which case default attributes are used.
+     * @param center      the circle's center location.
+     * @param radius      the circle's radius, in meters.
+     * @param intervals   the initial number of intervals (or slices) defining the circle's geometry.
+     * @param theta       the angle defining the start and end of the circle's geometry.
+     *
+     * @throws IllegalArgumentException if the center is null, if the radius is negative, or if the number of intervals
+     *                                  is less than 8.
+     */
+    public SurfaceCircle(ShapeAttributes normalAttrs, LatLon center, double radius, int intervals, Angle theta)
+    {
+        super(normalAttrs, center, radius, radius, Angle.ZERO, intervals, theta);
+    }
 
     public double getRadius()
     {
