157d56b4e458a7360018ddcdcc57b3f30bd29b27
mainstringargs <mainstringargs@users.noreply.github.com>
2019-04-19 09:27:43 -0500

    Fixing spacing from tabs

--- src/gov/nasa/worldwind/render/PointPlacemark.java
+++ src/gov/nasa/worldwind/render/PointPlacemark.java
@@ -977,24 +977,24 @@ public class PointPlacemark extends WWObjectImpl
                     (byte) color.getAlpha());
             }
 
-			// Compute the scale
-			double xscale;
-			Double scale = this.getActiveAttributes().getScale();
-			if (scale != null)
-				xscale = scale * this.activeTexture.getWidth(dc);
-			else
-				xscale = this.activeTexture.getWidth(dc);
+            // Compute the scale
+            double xscale;
+            Double scale = this.getActiveAttributes().getScale();
+            if (scale != null)
+                xscale = scale * this.activeTexture.getWidth(dc);
+            else
+                xscale = this.activeTexture.getWidth(dc);
 
-			double yscale;
-			if (scale != null)
-				yscale = scale * this.activeTexture.getHeight(dc);
-			else
-				yscale = this.activeTexture.getHeight(dc);
-			double maxwh = Math.max(xscale, yscale);
+            double yscale;
+            if (scale != null)
+                yscale = scale * this.activeTexture.getHeight(dc);
+            else
+                yscale = this.activeTexture.getHeight(dc);
+            double maxwh = Math.max(xscale, yscale);
 
-			// The image is drawn using a parallel projection.
-			osh.pushProjectionIdentity(gl);
-			gl.glOrtho(0d, dc.getView().getViewport().width, 0d, dc.getView().getViewport().height, -0.6 * maxwh, 0.6 * maxwh);
+            // The image is drawn using a parallel projection.
+            osh.pushProjectionIdentity(gl);
+            gl.glOrtho(0d, dc.getView().getViewport().width, 0d, dc.getView().getViewport().height, -0.6 * maxwh, 0.6 * maxwh);
 
             // Apply the depth buffer but don't change it (for screen-space shapes).
             if ((!dc.isDeepPickingEnabled()))
