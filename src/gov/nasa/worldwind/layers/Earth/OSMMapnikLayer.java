dcf0956b9dd3b28a100046d04227093d46f4c300
Wiehann Matthysen <wcmatthysen@gmail.com>
2019-04-24 20:43:21 +0200

    Refactored OSMMapnikLayer.
    Refactored the OSMMapnikLayer class to make use of the modified
    superclass constructor. Thus, we no longer have our own makeLevels()
    method, but instead we call the super-class constructor, relying on the
    makeLevels() method there to correctly populate the AVList parameters.
    The internal URLBuilder class was also modified to extend from
    MercatorTileUrlBuilder instead of TileUrlBuilder. This means that the
    getMercatorURL() method had to be implemented.

--- src/gov/nasa/worldwind/layers/Earth/OSMMapnikLayer.java
+++ src/gov/nasa/worldwind/layers/Earth/OSMMapnikLayer.java
@@ -5,10 +5,7 @@
  */
 package gov.nasa.worldwind.layers.Earth;
 
-import gov.nasa.worldwind.avlist.*;
-import gov.nasa.worldwind.geom.*;
 import gov.nasa.worldwind.layers.mercator.*;
-import gov.nasa.worldwind.util.*;
 
 import java.net.*;
 
@@ -19,37 +16,15 @@ public class OSMMapnikLayer extends BasicMercatorTiledImageLayer
 {
     public OSMMapnikLayer()
     {
-        super(makeLevels());
+        super("h", "Earth/OSM-Mercator/OpenStreetMap Mapnik", 19, 256, false, ".png", new URLBuilder());
     }
 
-    private static LevelSet makeLevels()
+    private static class URLBuilder extends MercatorTileUrlBuilder
     {
-        AVList params = new AVListImpl();
-
-        params.setValue(AVKey.TILE_WIDTH, 256);
-        params.setValue(AVKey.TILE_HEIGHT, 256);
-        params.setValue(AVKey.DATA_CACHE_NAME, "Earth/OSM-Mercator/OpenStreetMap Mapnik");
-        params.setValue(AVKey.SERVICE, "http://a.tile.openstreetmap.org/");
-        params.setValue(AVKey.DATASET_NAME, "h");
-        params.setValue(AVKey.FORMAT_SUFFIX, ".png");
-        params.setValue(AVKey.NUM_LEVELS, 16);
-        params.setValue(AVKey.NUM_EMPTY_LEVELS, 0);
-        params.setValue(AVKey.LEVEL_ZERO_TILE_DELTA, new LatLon(Angle
-            .fromDegrees(22.5d), Angle.fromDegrees(45d)));
-        params.setValue(AVKey.SECTOR, new MercatorSector(-1.0, 1.0, Angle.NEG180, Angle.POS180));
-        params.setValue(AVKey.TILE_URL_BUILDER, new URLBuilder());
-
-        return new LevelSet(params);
-    }
-
-    private static class URLBuilder implements TileUrlBuilder
-    {
-        public URL getURL(Tile tile, String imageFormat)
-            throws MalformedURLException
+        @Override
+        protected URL getMercatorURL(int x, int y, int z) throws MalformedURLException
         {
-            return new URL(tile.getLevel().getService()
-                + (tile.getLevelNumber() + 3) + "/" + tile.getColumn() + "/"
-                + ((1 << (tile.getLevelNumber()) + 3) - 1 - tile.getRow()) + ".png");
+            return new URL("https://a.tile.openstreetmap.org/" + z + "/" + x + "/" + y + ".png");
         }
     }
 
