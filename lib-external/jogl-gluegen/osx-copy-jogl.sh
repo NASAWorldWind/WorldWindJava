2e215a4aaf2f181a507cca842abd338c479623b1
markpet49 <mpeterson@farmintelligence.com>
2019-08-29 17:48:07 -0500

    JOGL for OSX

---
+++ lib-external/jogl-gluegen/osx-copy-jogl.sh
@@ -0,0 +1,7 @@
+set -x 
+
+cp gluegen/build/gluegen-rt-natives-macosx-universal.jar ../..
+cp gluegen/build/gluegen-rt.jar ../..
+cp jogl/build/jar/jogl-all.jar ../..
+cp jogl/build/jar/jogl-all-natives-macosx-universal.jar ../..
+
