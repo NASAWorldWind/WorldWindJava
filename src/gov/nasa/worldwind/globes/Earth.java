fe72f8bc8b3e458d5bb2b133d7e7ae4b96cb462f
Miguel Del Castillo <migueldelcastillo@live.com>
2020-06-19 18:32:18 -0500

    Feature/lunar ellipsoid (#1)
    * testing spherical ellipsoid for earth
    
    * modified earth ellipsoid to fit IAU 2000 lunar parameters
    
    * Modified Blue Marble Layer Configuration File to Retrieve Lunar Imagery from USGS
    
    Co-authored-by: Lisette Melendez <lisettemelendez@Lisettes-MBP.ndc.nasa.gov>

--- src/gov/nasa/worldwind/globes/Earth.java
+++ src/gov/nasa/worldwind/globes/Earth.java
@@ -17,9 +17,9 @@ import gov.nasa.worldwind.avlist.AVKey;
 
 public class Earth extends EllipsoidalGlobe
 {
-    public static final double WGS84_EQUATORIAL_RADIUS = 6378137.0; // ellipsoid equatorial getRadius, in meters
-    public static final double WGS84_POLAR_RADIUS = 6356752.3; // ellipsoid polar getRadius, in meters
-    public static final double WGS84_ES = 0.00669437999013; // eccentricity squared, semi-major axis
+    public static final double WGS84_EQUATORIAL_RADIUS = 1737400; // ellipsoid equatorial getRadius, in meters
+    public static final double WGS84_POLAR_RADIUS = 1737400; // ellipsoid polar getRadius, in meters
+    public static final double WGS84_ES = 0.0; // eccentricity squared, semi-major axis
 
     public static final double ELEVATION_MIN = -11000d; // Depth of Marianas trench
     public static final double ELEVATION_MAX = 8500d; // Height of Mt. Everest.
